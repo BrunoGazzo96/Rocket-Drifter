ac76f07eec62191feba362c7786f5dd5f5cb5a4d
Bruno Gazzo <developer1@kerberos.com.ar>
2023-09-16 16:54:19 -0300

    Nuevo nivel y manejo de escenas

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,3 +1,4 @@
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
@@ -9,12 +10,36 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("Iniciando");
                 break;
-            case "Fuel":
-                Debug.Log("Recargando");
-                break;
             case "Finish":
-                Debug.Log("Llegaste");
+                LoadNextLevel();
+                break;
+            default:
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        GetComponent<AudioSource>().Pause();
+        Invoke(nameof(ReloadLevel), 1f);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
